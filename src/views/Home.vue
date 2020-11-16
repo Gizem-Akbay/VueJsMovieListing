1eaea5da1fe669f6553bf0055b4a324caaf65883
gizemakbay1997@gmail.com <gizemakbay1997@gmail.com>
2020-11-16 12:16:49 +0300

    updated components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,20 @@
 <template>
     <div>
-        <appNavbar ></appNavbar>
+        <appNavbar></appNavbar>
         <div class="pusher">
             <appSlider></appSlider>
-            <appMovieList></appMovieList>
             <appFooter></appFooter>
-
         </div>
     </div>
 </template>
 <script>
     import Navbar from "./Navbar";
     import Slider from "./Slider";
-    import MovieList from "./MovieList";
     import Footer from "./Footer";
     export default {
     name: 'Home',
         components: {
             appNavbar: Navbar,
-            appMovieList: MovieList,
             appFooter: Footer,
             appSlider:Slider
         }

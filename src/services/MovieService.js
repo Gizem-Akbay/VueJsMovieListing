7f81d2f5242701485cebed3ae8afaba7cb4c6e91
gizemakbay1997@gmail.com <gizemakbay1997@gmail.com>
2020-11-16 16:11:03 +0300

    edited api url

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
-
-const API_URL = "https://yts.mx/api/v2/list_movies.json?"
-const TEST = "http://omdbapi.com/?apikey=21422052&"
-
+const API_BASE_URL = "https://yts.mx/api/v2/list_movies.json?"
 export default {
-    get_title(val) {
-        var query = "sort_by=year&limit=20";
-
+    get_movies(val, mode) {
+        var query = "limit=20";
+        if (mode === 'last') {
+            query += '&sort_by=year'
+        } else if (mode === 'like') {
+            query += '&sort_by=rating'
+        }
         if (val) {
             query += ("&query_term=" + val)
         }
-        return axios.get(API_URL + query);
-    },
-    test() {
-        return axios.get(TEST + 's=hangover&plot=full');
+        return axios.get(API_BASE_URL + query);
     }
 };
